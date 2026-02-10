074dd798fd7f493c84e6af22f31aeb960020ac11
SRIHARIRAGHAV <135862963+SRIHARIRAGHAV@users.noreply.github.com>
2026-02-10 11:03:17 +0530

    Update mediapipe_service.py

--- app/services/mediapipe_service.py
+++ app/services/mediapipe_service.py
@@ -1,4 +1,3 @@
-
 import mediapipe as mp
 import cv2
 
@@ -8,11 +7,20 @@ mp_hands = mp.solutions.hands.Hands(
     min_detection_confidence=0.7
 )
 
-def extract_landmarks(frame):
+def detect_hand(frame):
+    h, w, _ = frame.shape
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = mp_hands.process(rgb)
 
     if not result.multi_hand_landmarks:
         return None
 
-    return [[lm.x, lm.y, lm.z] for lm in result.multi_hand_landmarks[0].landmark]
+    hand_landmarks = result.multi_hand_landmarks[0]
+
+    x_coords = [int(lm.x * w) for lm in hand_landmarks.landmark]
+    y_coords = [int(lm.y * h) for lm in hand_landmarks.landmark]
+
+    x1, y1 = min(x_coords), min(y_coords)
+    x2, y2 = max(x_coords), max(y_coords)
+
+    return (x1, y1, x2, y2)
